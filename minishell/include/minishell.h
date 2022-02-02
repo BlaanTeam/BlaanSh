a82d0266a8f60f5184a22e916afc99a1e590bc21
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2022-02-02 15:51:51 +0100

    chore: add minishell header file

---
+++ minishell/include/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 16:41:47 by asabani           #+#    #+#             */
+/*   Updated: 2022/02/02 15:51:24 by asabani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include "readline/readline.h"
+# include "readline/history.h"
+# include <sys/types.h>
+# include <signal.h>
+
+/*
+grammer rules :
+	cmdline:  (pipeline [io_modifier]) | comment | varible_assignment | WHITESPACE
+	pipeline : pipeline [PIPE cmd_and_args] | cmd_and_args | EMPTY
+	cmd_and_args : WHITESPACE (cmd_name | cmd_path) cmd_arg*
+	cmd_name : WORD
+	cmd_path : PATH
+	cmd_arg: WORD
+	comment : HASH TEXT
+*/
+
+#endif
