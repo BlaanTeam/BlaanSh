182aa65bbc456a0c4a6282b9e9cb0842efb0fc7d
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-08 18:55:54 +0100

    refactor: updated garbage collector header file
    - Added GC_DESTROY_SELF utility

--- libgc/include/gc.h
+++ libgc/include/gc.h
@@ -3,18 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   gc.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 00:29:24 by asabani           #+#    #+#             */
-/*   Updated: 2022/02/06 00:29:37 by asabani          ###   ########.fr       */
+/*   Updated: 2022/02/08 18:53:36 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GC_H
 # define GC_H
+
 # include <stdlib.h>
-# define GC_ALL	1U
-# define GC_TMP	2U
+
+# define GC_ALL				1U
+# define GC_TMP				2U
+# define GC_DESTROY_SELF	4U
+
+typedef unsigned short	t_gc_flag;
 
 typedef struct s_dustbin
 {
@@ -22,14 +27,13 @@ typedef struct s_dustbin
 	struct s_dustbin	*next;
 }	t_dustbin;
 
-typedef unsigned short	t_gc_flag;
 typedef struct s_gc
 {
 	t_dustbin	*dustbin;
 	t_dustbin	*tmp_dustbin;
 	struct s_gc	*(*append)(struct s_gc *, void *, t_gc_flag);
 	void		(*clean)(struct s_gc **, t_gc_flag);
-}	t_gc;
+}				t_gc;
 
 t_gc	*gc_init(void);
 t_gc	*gc_append(t_gc *self, void *garbage, t_gc_flag flag);
