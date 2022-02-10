28da87b1a4ed57c811b2ac0fd01a501fa0fe46eb
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2022-02-10 23:26:23 +0100

    refactor: clean up before exit

--- src/builtin_commands/exit_cmd.c
+++ src/builtin_commands/exit_cmd.c
@@ -6,7 +6,7 @@
 /*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 16:23:39 by asabani           #+#    #+#             */
-/*   Updated: 2022/02/10 17:03:16 by asabani          ###   ########.fr       */
+/*   Updated: 2022/02/10 20:15:55 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 void	ft_exit(char **av)
 {
 	if (!av[0])
-	{
-		gc_clean(&g_global.gc, GC_DESTROY_SELF);
-		rl_clear_history();
-		exit(EXIT_SUCCESS);
-	}
+		exit_with_cleanup();	
 	if (!is_numeric(av[0]))
 		return (cmd_error("exit", av[0], "numeric argument required"), \
-		exit(255));
+		exit_with_code(255, NULL, true));
 	else if (av[1])
 		return (cmd_error("exit", "too many arguments", NULL));
 	else
-		exit(ft_atoi(av[0]));
+		exit_with_code(ft_atoi(av[0]), NULL, true);
 }
