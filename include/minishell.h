fa6656d3c13722db4b976a78c11e3e3e80359e41
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-15 16:02:54 +0100

    refactor: updated header file
    - Updated token names (powers of two) to be used like flags
    - Added STRING token

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:41:47 by asabani           #+#    #+#             */
-/*   Updated: 2022/02/12 23:52:51 by asabani          ###   ########.fr       */
+/*   Updated: 2022/02/14 20:56:19 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,28 +43,31 @@ grammer rules :
 // tokens
 typedef enum e_token
 {
-	WHITESPACE,
-	TK_NEWLINE,
-	WORD,
-	PATH,
-	PIPE,
-	AND_IF,
-	OR_IF,
-	O_PARENTHESESE,
-	C_PARENTHESESE,
-	EQUAL,
-	AMPERSAND,
-	SEMICL,
-	DSEMICL,
-	VAR_EXPANSION,
-	TILDE_EXPANSION,
-	WILDCARD_EXPANSION,
-	SINGLE_QUOTE,
-	DOUBLE_QUOTE,
-	LESS,
-	DLESS,
-	GREAT,
-	DGREAT
+	CMDBEGIN = 1 << 0,
+	ENDOFCMD = 1 << 1,
+	WHITESPACE = 1 << 2,
+	WORD = 1 << 3,
+	PATH =  1 << 4,
+	PIPE =  1 << 5,
+	AND_IF =  1 << 6,
+	OR_IF = 1 << 7,
+	O_PARENTHESESE = 1 << 8,
+	C_PARENTHESESE = 1 << 9,
+	EQUAL = 1 << 10,
+	ASSIGNMENT = 1 << 11,
+	AMPERSAND = 1 << 12,
+	SEMICL = 1 << 13,
+	DSEMICL = 1 << 14,
+	VAR_EXPANSION = 1 << 15,
+	TILDE_EXPANSION = 1 << 16,
+	WILDCARD_EXPANSION = 1 << 17,
+	SINGLE_QUOTE = 1 << 18,
+	DOUBLE_QUOTE = 1 << 19,
+	LESS = 1 << 20,
+	DLESS = 1 << 21,
+	GREAT = 1 << 22,
+	DGREAT = 1 << 23,
+	STRING = (WORD | PATH | TILDE_EXPANSION | ASSIGNMENT | WILDCARD_EXPANSION | SINGLE_QUOTE | DOUBLE_QUOTE)
 }	t_token;
 
 typedef struct s_node
@@ -133,7 +136,7 @@ void	alloc_error(void);
 void	cmd_error(char *cmd, char *msg, char *extra);
 
 /*
-**	doubly linked list utils
+**	parsing utils
 */
 t_list	*new_list(void);
 void	push_front(t_list *list, t_token token, char *val);
@@ -143,6 +146,7 @@ int		del_back(t_list *list);
 void	list_clear(t_list *list);
 
 t_list	*tokenize(char *line);
+bool	validate_syntax(t_list *tokens);
 
 // builtin commands
 void	cd(char **av, t_venv **venv);
