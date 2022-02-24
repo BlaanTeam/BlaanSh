1ad92d7098bb6042a0362bce3e9d5a9a65dd07dc
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-24 17:34:57 +0100

    refactor: add list constructors/destructors utils

--- src/tokenizer/list_constructor_utils.c
+++ src/tokenizer/list_constructor_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   list_utils.c                                       :+:      :+:    :+:   */
+/*   list_constructor_utils.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 14:09:45 by omoussao          #+#    #+#             */
-/*   Updated: 2022/02/19 15:10:49 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/02/24 17:32:55 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,52 +85,4 @@ t_node	*insert_node(t_list *list, t_node *new, t_node *prev)
 		prev->next = new;
 	}
 	return (new);
-}
-
-t_node	*del_front(t_list *list)
-{
-	t_node	*ret;
-
-	if (list->len == 0)
-		return (NULL);
-	ret = list->top;
-	list->top = list->top->next;
-	if (list->top)
-		list->top->prev = NULL;
-	(list->len)--;
-	return (ret);
-}
-
-t_node	*del_back(t_list *list)
-{
-	t_node	*ret;
-
-	if (list->len == 0)
-		return (NULL);
-	ret = list->bottom;
-	list->bottom = list->bottom->prev;
-	if (list->bottom)
-		list->bottom->next = NULL;
-	(list->len)--;
-	return (ret);
-}
-
-t_node	*del_node(t_list *list, t_node *node)
-{
-	if (!node)
-		return (NULL);
-	if (!node->prev)
-		return (del_front(list));
-	if (!node->next)
-		return (del_back(list));
-	(list->len)--;
-	node->prev->next = node->next;
-	node->next->prev = node->prev;
-	return (node);
-}
-
-void	list_clear(t_list *list)
-{
-	while (list->len)
-		del_front(list);
 }
---
+++ src/tokenizer/list_destructor_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_destructor_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 17:31:42 by omoussao          #+#    #+#             */
+/*   Updated: 2022/02/24 17:32:01 by omoussao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_node	*del_front(t_list *list)
+{
+	t_node	*ret;
+
+	if (list->len == 0)
+		return (NULL);
+	ret = list->top;
+	list->top = list->top->next;
+	if (list->top)
+		list->top->prev = NULL;
+	(list->len)--;
+	return (ret);
+}
+
+t_node	*del_back(t_list *list)
+{
+	t_node	*ret;
+
+	if (list->len == 0)
+		return (NULL);
+	ret = list->bottom;
+	list->bottom = list->bottom->prev;
+	if (list->bottom)
+		list->bottom->next = NULL;
+	(list->len)--;
+	return (ret);
+}
+
+t_node	*del_node(t_list *list, t_node *node)
+{
+	if (!node)
+		return (NULL);
+	if (!node->prev)
+		return (del_front(list));
+	if (!node->next)
+		return (del_back(list));
+	(list->len)--;
+	node->prev->next = node->next;
+	node->next->prev = node->prev;
+	return (node);
+}
+
+void	list_clear(t_list *list)
+{
+	while (list->len)
+		del_front(list);
+}
