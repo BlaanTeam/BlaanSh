f5b67039df714043320a45b6be11b770c3de516a
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-26 17:04:25 +0100

    refactor: update syntax_analyzer
    - Correct wrong token names
    - Update syntax rules
    - Update check_statement logic

--- src/syntax_analyser/syntax_analyser.c
+++ src/syntax_analyser/syntax_analyser.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 16:24:15 by omoussao          #+#    #+#             */
-/*   Updated: 2022/02/24 19:33:17 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/02/26 17:02:49 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 grammer rules :
 	
 	! I-GRAMMAR:
-	cmdline := pipeline | pipeline AND_IF cmdline | pipeline OR_IF cmdline
+	cmdline := pipeline | pipeline AND cmdline | pipeline OR cmdline
 
 	pipeline := command | command PIPE pipeline
 	
@@ -34,22 +34,22 @@ grammer rules :
 
 	! II-SYNTAX:
 	* UNEXPECTED TOKENS:
-		- `;' | `;;' | `&'
+		- `;;' (this token is always unrecognized in bash and zsh shells)
 
-	* AND_IF, OR_IF, PIPE:
-		- left: [WHITESPACE] (STRING | CPAR)
-		- right: [WHITESPACE] (STRING | REDIRECT | OPAR)
+	* AND, OR, PIPE, FG, BG:
+		- left: [WSPACE] (STRING | CPAR)
+		- right: [WSPACE] (STRING | REDIRECT | OPAR | if <FG, BG> ENDOFCMD)
 
 	* OPAR "(":
-		- left: CMDBEGIN | [WHITESPACE] (AND_IF | OR_IF | PIPE | OPAR)
-		- right: [WHITESPACE] (STRING | REDIRECT | OPAR)
+		- left: CMDBEGIN | [WSPACE] (AND | OR | PIPE | OPAR)
+		- right: [WSPACE] (STRING | REDIRECT | OPAR)
 
 	* CPAR ")":
-		- left: [WHITESPACE] (STRING | CPAR)
-		- right: [WHITESPACE] (AND_IF | OR_IF | PIPE | CPAR | ENDOFCMD)
+		- left: [WSPACE] (STRING | CPAR)
+		- right: [WSPACE] (AND | OR | PIPE | CPAR | ENDOFCMD)
 
 	* REDIRECT:
-		- right: [WHITESPACE] STRING
+		- right: [WSPACE] STRING
 
 	* PARENTHESES MATCHING AND QUOTING:
 		- inside each pair parentheses sould not be an empthy command
@@ -62,7 +62,7 @@ t_node	*get_right(t_node *tokp)
 	t_node	*right;
 
 	right = tokp->next;
-	while (right->token == WHITESPACE)
+	while (right->token == WSPACE)
 		right = right->next;
 	return (right);
 }
@@ -72,7 +72,7 @@ t_node	*get_left(t_node *tokp)
 	t_node	*left;
 
 	left = tokp->prev;
-	while (left->token == WHITESPACE)
+	while (left->token == WSPACE)
 		left = left->prev;
 	return (left);
 }
@@ -82,8 +82,7 @@ bool	check_unexpected(t_node *tokp)
 	t_token	tok;
 
 	tok = tokp->token;
-	// if (tok == AMPERSAND || tok == SEMICL || tok == DSEMICL)
-	if (tok & (AMPERSAND | SEMICL | DSEMICL))
+	if (tok & DSEMI)
 	{
 		fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", tokp->val);
 		return (false);
@@ -98,16 +97,18 @@ bool	check_statements(t_node *tokp)
 	t_token	tok;
 
 	tok = tokp->token;
-	if (!(tok & (PIPE | OR_IF | AND_IF)))
+	if (!(tok & OP))
 		return (true);
 	left = get_left(tokp);
 	right = get_right(tokp);
-	if (!(left->token & (STRING | C_PARENTHESESE)))
+	if (!(left->token & (STRING | CPAR)))
 	{
 		fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", tokp->val);
 		return (false);
 	}
-	else if (!(right->token & (STRING | O_PARENTHESESE | REDIRECT)))
+	else if ((tok & (FG | BG)) && right->token == ENDOFCMD)
+		return (true);
+	else if (!(right->token & (STRING | OPAR | REDIRECT)))
 	{
 		fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", right->val);
 		return (false);
@@ -122,31 +123,31 @@ bool	check_parentheses(t_node *tokp)
 	t_token	tok;
 
 	tok = tokp->token;
-	if (tok != O_PARENTHESESE && tok != C_PARENTHESESE)
+	if (tok != OPAR && tok != CPAR)
 		return (true);
 	left = get_left(tokp);
 	right = get_right(tokp);
-	if (tok == O_PARENTHESESE)
+	if (tok == OPAR)
 	{
-		if (!(left->token & (CMDBEGIN | AND_IF | OR_IF | PIPE | O_PARENTHESESE)))
+		if (!(left->token & (CMDBEGIN | OP | OPAR)))
 		{
 			fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", tokp->val);
 			return (false);
 		}
-		else if (!(right->token & (STRING | O_PARENTHESESE | REDIRECT)))
+		else if (!(right->token & (STRING | OPAR | REDIRECT)))
 		{
 			fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", right->val);
 			return (false);
 		}
 	}
-	else if (tok == C_PARENTHESESE)
+	else if (tok == CPAR)
 	{
-		if (!(left->token & (STRING | C_PARENTHESESE)))
+		if (!(left->token & (STRING | CPAR)))
 		{
 			fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", tokp->val);
 			return (false);
 		}
-		else if (!(right->token & (ENDOFCMD | AND_IF | OR_IF | PIPE | C_PARENTHESESE | REDIRECT)))
+		else if (!(right->token & (ENDOFCMD | OP | CPAR | REDIRECT)))
 		{
 			fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", right->val);
 			return (false);
@@ -169,7 +170,7 @@ bool	check_redirections(t_node *tokp)
 		fprintf(stderr, "minishell: syntax error near unexpected token `%s'\n", right->val);
 		return (false);
 	}
-	else if (right->token == VAR_EXPANSION && (!getvenv(right->val) || !*getvenv(right->val)))
+	else if (right->token == VAR && (!getvenv(right->val) || !*getvenv(right->val)))
 	{
 		fprintf(stderr, "minishell: $%s: ambiguous redirect\n", right->val);
 		return (false);
@@ -190,10 +191,10 @@ bool	check_matching_quotes_parentheses(t_list *tokens)
 	top = tokens->top;
 	while (top->token != ENDOFCMD)
 	{
-		par_matching += (top->token == O_PARENTHESESE);
-		par_matching -= (top->token == C_PARENTHESESE);
-		dquotes += (top->token == DOUBLE_QUOTE);
-		squotes += (top->token == SINGLE_QUOTE);
+		par_matching += (top->token == OPAR);
+		par_matching -= (top->token == CPAR);
+		dquotes += (top->token == DQUOTE);
+		squotes += (top->token == SQUOTE);
 		if (par_matching < 0)
 			return (fprintf(stderr, "minishell: syntax error near unexpected token `)'\n"), false);
 		top = top->next;
