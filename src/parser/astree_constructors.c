15e49e71c1d96b304305d0b5746ce546f85d8824
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-27 19:38:20 +0100

    refactor: update ASTree constructors
    - Change return type of all constructors to t_cmdtree pointer

--- src/parser/astree_constructors.c
+++ src/parser/astree_constructors.c
@@ -6,26 +6,26 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 16:49:07 by omoussao          #+#    #+#             */
-/*   Updated: 2022/02/27 16:53:26 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/02/27 19:36:39 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // Subshell node constructor
-t_subsh	*new_subsh(t_cmdtree *subtree)
+t_cmdtree	*new_subsh(t_cmdtree *subtree)
 {
 	t_subsh	*subshell;
 
 	subshell = gc_filter(malloc(sizeof(t_subsh)), GC_TMP);
 	subshell->node_type = NODE_SUBSH;
 	subshell->cmdtree = subtree;
-	return (subshell);
+	return ((t_cmdtree *)subshell);
 }
 
 // Connector node constructor
 // Connector might be: AND, OR, PIPE, FG, BG
-t_connector	*new_connector(int node_type, t_cmdtree *left, t_cmdtree *right)
+t_cmdtree	*new_connector(int node_type, t_cmdtree *left, t_cmdtree *right)
 {
 	t_connector	*connector;
 
@@ -33,11 +33,11 @@ t_connector	*new_connector(int node_type, t_cmdtree *left, t_cmdtree *right)
 	connector->node_type = node_type;
 	connector->lcmdtree = left;
 	connector->rcmdtree = right;
-	return (connector);
+	return ((t_cmdtree *)connector);
 }
 
 // Command list node constructor
-t_cmdlist	*new_cmdlist(t_list *cmdvec)
+t_cmdtree	*new_cmdlist(t_list *cmdvec)
 {
 	t_cmdlist	*cmdlist;
 
@@ -45,18 +45,18 @@ t_cmdlist	*new_cmdlist(t_list *cmdvec)
 	cmdlist->node_type = NODE_CMDLST;
 	cmdlist->cmdvec = new_list();
 	cmdlist->cmdargs = NULL;
-	return (cmdlist);
+	return ((t_cmdtree *)cmdlist);
 }
 
 // Redirect node constructor
 // Fill only node_type, and cmdtree pointer
 // Other data will depend on parse_redir
-t_redir	*new_redir(t_cmdtree *cmdtree)
+t_cmdtree	*new_redir(t_cmdtree *cmdtree)
 {
 	t_redir	*redir;
 
 	redir = gc_filter(malloc(sizeof(t_redir)), GC_TMP);
 	redir->node_type = NODE_REDIR;
 	redir->cmdtree = cmdtree;
-	return (redir);
+	return ((t_cmdtree *)redir);
 }
