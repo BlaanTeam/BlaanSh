521a9f45aa1238e9e157f4339bef9ab5c0e33185
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-28 15:46:51 +0100

    refactor: delete disp from minishell.c

--- src/minishell.c
+++ src/minishell.c
@@ -3,56 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 22:13:08 by asabani           #+#    #+#             */
-/*   Updated: 2022/02/28 00:41:08 by asabani          ###   ########.fr       */
+/*   Updated: 2022/02/28 15:41:02 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//* names of tokens (for debuging)
-const char *token_names[] = {
-	"CMDBEGIN",
-	"ENDOFCMD",
-	"WSPACE",
-	"WORD",
-	"PATH",
-	"PIPE",
-	"AND",
-	"OR",
-	"OPAR",
-	"CPAR",
-	"BG",
-	"FG",
-	"DSEMI",
-	"VAR",
-	"TILDE",
-	"WILDC",
-	"SQUOTE",
-	"DQUOTE",
-	"LESS",
-	"DLESS",
-	"GREAT",
-	"DGREAT",
-};
-
 t_global	g_global = {.gc = 0, \
 						.program_name = NULL, \
 						.status = 0, \
 						.is_running = 0};
-
-#include <string.h>
-#include <math.h>
-void	disp(t_node *top)
-{
-	while (top)
-	{
-		printf("tok = %-40s val = %s\n", token_names[(int)log2(top->token)], top->val);
-		top = top->next;
-	}
-}
 
 int	main(int ac, char **av, char **env)
 {
