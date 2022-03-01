d3db580ee2925c079c75cac635dfb196703f7f0a
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-03-01 19:25:06 +0100

    refactor: update parser.c
    - Delete grammar
    - Protect from tokens == NULL
    - fix _error() usage

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,44 +6,27 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 17:09:44 by omoussao          #+#    #+#             */
-/*   Updated: 2022/02/28 16:30:14 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/03/01 17:14:59 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// The grammar recognized by this parser:
-
-// <cmdline>    ::= <block>
-//              |   <block> (";" | "&") <cmdline>
-// 
-// <block>      ::=  <pipeline> {("&&" | "||") <pipeline>}
-//
-// <pipeline>   ::=  <command> {"|" <command>}
-//
-// <command>    ::=  <cmdlist>
-//              |    "(" <cmdline> ")" <redir>  (* subshell *)
-//
-// <cmdlist>    ::=  <redir>+                    (* at least one redirect - without WORDs *)
-//              |    <redir> {<arg> <redir>}+    (* at least one WORD - zero or more <redir> in any place *)
-//
-// <redir>      ::=  {("<" | "<<" | ">" | ">>") <filename>}
-//
-// <arg> <filename> ::= token WORD
-
 // Parser
 t_cmdtree	*parser(t_lexer *tokens)
 {
-	t_cmdtree	*abstract_syntax_tree;
+	t_cmdtree	*syntax_tree;
 	t_node		*tokp;
 
+	if (!tokens || tokens->len <= 1)
+		return (NULL);
 	tokp = tokens->top;
-	abstract_syntax_tree = parse_cmdline(&tokp);
-	if (!abstract_syntax_tree
+	syntax_tree = parse_cmdline(&tokp);
+	if (!syntax_tree
 		|| (tokp && current(tokp) != ENDOFCMD))
 	{
-		_error("Parser: Syntax Error", 158);
+		_error("parser", "Syntax Error", NULL, 2);
 		return (NULL);
 	}
-	return (abstract_syntax_tree);
+	return (syntax_tree);
 }
