0857e81522c722749c9774562b27a47a45de2b42
Abdelhadi Sabani <abdo.sabani30@gmail.com>
2022-03-03 01:01:04 +0100

    refactor: save original `STDOUT` & `STDIN`

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: asabani <asabani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 22:13:08 by asabani           #+#    #+#             */
-/*   Updated: 2022/03/02 00:46:11 by asabani          ###   ########.fr       */
+/*   Updated: 2022/03/03 00:19:05 by asabani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ t_global	g_global = {.gc = NULL, \
 
 int	main(int ac, char **av, char **env)
 {
+	int			io[2];
 	char		*cmdline;
 	t_cmdtree	*tree;
 
@@ -28,6 +29,8 @@ int	main(int ac, char **av, char **env)
 		exit_with_code(EXIT_FAILURE, "malloc", false);
 	set_program_name(av[0]);
 	g_global.venv = venv_init(env);
+	io[0] = dup(STDIN_FILENO);
+	io[1] = dup(STDOUT_FILENO);
 	while (true)
 	{
 		term_init();
@@ -38,8 +41,11 @@ int	main(int ac, char **av, char **env)
 		tree = parser(lexer(cmdline));
 		g_global.is_running = 1;
 		executor(tree);
+		// display_tree(tree, 0);
 		g_global.is_running = 0;
 		gc_clean(&g_global.gc, GC_TMP);
+		dup2(io[0], STDIN_FILENO);
+		dup2(io[1], STDOUT_FILENO);
 	}
 	exit_with_cleanup();
 }
