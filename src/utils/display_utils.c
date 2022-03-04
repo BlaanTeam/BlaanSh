f625874310103078e07a16216dda4d6c7ef6c436
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-03-04 19:51:45 +0100

    refactor: update display utils
    - Add GROUP in token_names
    - Update display_tokens logic (add GROUPs display)

--- src/utils/display_utils.c
+++ src/utils/display_utils.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 15:41:26 by omoussao          #+#    #+#             */
-/*   Updated: 2022/03/02 21:06:03 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/03/04 19:06:53 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ const char *token_names[] = {
 	"DLESS",
 	"GREAT",
 	"DGREAT",
+	"GROUP"
 };
 
 const char *type[] = {
@@ -55,23 +56,36 @@ const char *type[] = {
 
 void	display_tokens(t_node *top)
 {
-	printf("-----------------Display Tokens-----------------\n");
+	fprintf(stderr, "-----------------Display Tokens-----------------\n");
 	while (top)
 	{
-		printf("tok = %-40s val = %s\n", token_names[(int)log2(top->token)], top->val);
+		fprintf(stderr, "tok = %-40s ", token_names[(int)log2(top->token)]);
+		if (top->token != GROUP)
+			fprintf(stderr, "val = %s\n", top->val);
+		else
+		{
+			fprintf(stderr, "vals =");
+			t_node *cpy = top->val_grp->top;
+			while (cpy) {
+				fprintf(stderr, " <%s: %s> ", token_names[(int)log2(cpy->token)], cpy->val);
+				cpy = cpy->next;
+			}
+			fprintf(stderr, "\n");
+		}
 		top = top->next;
 	}
-	printf("-------------------Tokens End-------------------\n");
+	fprintf(stderr, "-------------------Tokens End-------------------\n");
 }
 
 void	display_venv(t_venv *venv)
 {
-	printf("-----------------The venv variables-----------------\n");
+	fprintf(stderr, "-----------------The venv variables-----------------\n");
 	while (venv)
 	{
-		printf("%s=%s\n", venv->key, venv->value);
+		fprintf(stderr, "%s=%s\n", venv->key, venv->value);
 		venv = venv->next;
 	}
+	fprintf(stderr, "-----------------END venv variables-----------------\n");
 }
 
 void	display_env(char **env)
@@ -82,42 +96,50 @@ void	display_env(char **env)
 		return ;
 	i = -1;
 	while (env[++i])
-		printf("%s\n", env[i]);
+		fprintf(stderr, "%s\n", env[i]);
 }
 
 // tree visualizer
-void display_tree(t_cmdtree *tree, int ident_level)
-{
-	if (tree == NULL)
-		return ;
-	for (int i = 0; i < ident_level; i++) {
-		fprintf(stderr, "├   ");
-	}
-	if (tree->node_type == NODE_CMDLST)
-	{
-		fprintf(stderr, "├── CMDLIST:");
-		t_node	*lst = ((t_cmdlist *)tree)->cmdvec->top;
-		while (lst) {
-			fprintf(stderr, " %s", lst->val);
-			lst = lst->next;
-		}
-		fprintf(stderr, "\n");
-	}
-	else if (tree->node_type == NODE_SUBSH) {
-		fprintf(stderr, "├── SUBSH\n");
-		display_tree(((t_subsh *)tree)->cmdtree, ident_level + 1);
-	}
-	else if (tree->node_type == NODE_REDIR) {
-		t_redir *redir = (t_redir *)tree;
-		fprintf(stderr, "REDIR %d to `%s`\n", redir->io_src, redir->filename);
-		display_tree(redir->cmdtree, ident_level + 1);
-	}
-	else if (tree->node_type == NODE_PIPE || tree->node_type == NODE_FG
-		|| tree->node_type == NODE_BG || tree->node_type == NODE_AND
-		|| tree->node_type == NODE_OR)
-	{
-		fprintf(stderr, "├── %s\n", type[tree->node_type]);
-		display_tree(((t_connector *)tree)->lcmdtree, ident_level + 1);
-		display_tree(((t_connector *)tree)->rcmdtree, ident_level + 1);
-	}
-}
+// void display_tree_(t_cmdtree *tree, int ident_level)
+// {
+// 	if (tree == NULL)
+// 		return ;
+// 	for (int i = 0; i < ident_level; i++) {
+// 		fprintf(stderr, "├   ");
+// 	}
+// 	if (tree->node_type == NODE_CMDLST)
+// 	{
+// 		fprintf(stderr, "├── CMDLIST:");
+// 		t_node	*lst = ((t_cmdlist *)tree)->cmdvec->top;
+// 		while (lst) {
+// 			fprintf(stderr, " %s: %s", lst->val, token_names[(int)log2(lst->token)]);
+// 			fprintf(stderr, " %s", lst->concat? "<->": "|");
+// 			lst = lst->next;
+// 		}
+// 		fprintf(stderr, "\n");
+// 	}
+// 	else if (tree->node_type == NODE_SUBSH) {
+// 		fprintf(stderr, "├── SUBSH\n");
+// 		display_tree_(((t_subsh *)tree)->cmdtree, ident_level + 1);
+// 	}
+// 	else if (tree->node_type == NODE_REDIR) {
+// 		t_redir *redir = (t_redir *)tree;
+// 		fprintf(stderr, "REDIR %d to `%s`\n", redir->io_src, redir->filename);
+// 		display_tree_(redir->cmdtree, ident_level + 1);
+// 	}
+// 	else if (tree->node_type == NODE_PIPE || tree->node_type == NODE_FG
+// 		|| tree->node_type == NODE_BG || tree->node_type == NODE_AND
+// 		|| tree->node_type == NODE_OR)
+// 	{
+// 		fprintf(stderr, "├── %s\n", type[tree->node_type]);
+// 		display_tree_(((t_connector *)tree)->lcmdtree, ident_level + 1);
+// 		display_tree_(((t_connector *)tree)->rcmdtree, ident_level + 1);
+// 	}
+// }
+
+// void	display_tree(t_cmdtree *tree)
+// {
+// 	fprintf(stderr, "-------------------Syntax Tree-------------------\n");
+// 	display_tree_(tree, 0);
+// 	fprintf(stderr, "-----------------End Syntax Tree-----------------\n");
+// }
