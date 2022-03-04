7235274ee8d0298b10b78d975262385b58f25554
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-03-04 22:32:16 +0100

    refactor: update display_tree

--- src/utils/display_utils.c
+++ src/utils/display_utils.c
@@ -6,7 +6,7 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 15:41:26 by omoussao          #+#    #+#             */
-/*   Updated: 2022/03/04 19:06:53 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/03/04 21:20:55 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,46 +100,68 @@ void	display_env(char **env)
 }
 
 // tree visualizer
-// void display_tree_(t_cmdtree *tree, int ident_level)
-// {
-// 	if (tree == NULL)
-// 		return ;
-// 	for (int i = 0; i < ident_level; i++) {
-// 		fprintf(stderr, "├   ");
-// 	}
-// 	if (tree->node_type == NODE_CMDLST)
-// 	{
-// 		fprintf(stderr, "├── CMDLIST:");
-// 		t_node	*lst = ((t_cmdlist *)tree)->cmdvec->top;
-// 		while (lst) {
-// 			fprintf(stderr, " %s: %s", lst->val, token_names[(int)log2(lst->token)]);
-// 			fprintf(stderr, " %s", lst->concat? "<->": "|");
-// 			lst = lst->next;
-// 		}
-// 		fprintf(stderr, "\n");
-// 	}
-// 	else if (tree->node_type == NODE_SUBSH) {
-// 		fprintf(stderr, "├── SUBSH\n");
-// 		display_tree_(((t_subsh *)tree)->cmdtree, ident_level + 1);
-// 	}
-// 	else if (tree->node_type == NODE_REDIR) {
-// 		t_redir *redir = (t_redir *)tree;
-// 		fprintf(stderr, "REDIR %d to `%s`\n", redir->io_src, redir->filename);
-// 		display_tree_(redir->cmdtree, ident_level + 1);
-// 	}
-// 	else if (tree->node_type == NODE_PIPE || tree->node_type == NODE_FG
-// 		|| tree->node_type == NODE_BG || tree->node_type == NODE_AND
-// 		|| tree->node_type == NODE_OR)
-// 	{
-// 		fprintf(stderr, "├── %s\n", type[tree->node_type]);
-// 		display_tree_(((t_connector *)tree)->lcmdtree, ident_level + 1);
-// 		display_tree_(((t_connector *)tree)->rcmdtree, ident_level + 1);
-// 	}
-// }
+void display_tree_(t_cmdtree *tree, int ident_level)
+{
+	if (tree == NULL)
+		return ;
+	for (int i = 0; i < ident_level; i++) {
+		fprintf(stderr, "├   ");
+	}
+	if (tree->node_type == NODE_CMDLST)
+	{
+		fprintf(stderr, "├── CMDLIST:");
+		t_node	*lst = ((t_cmdlist *)tree)->cmdvec->top;
+		while (lst) {
+			if (lst->token != GROUP)
+				fprintf(stderr, " <%s: %s> ", lst->val, token_names[(int)log2(lst->token)]);
+			else
+			{
+				t_node *cpy = lst->val_grp->top;
+				fprintf(stderr, " <GROUP: {");
+				while (cpy) {
+					fprintf(stderr, " <%s: %s> ", token_names[(int)log2(cpy->token)], cpy->val);
+					cpy = cpy->next;
+				}
+				fprintf(stderr, "}>");
+			}
+			lst = lst->next;
+		}
+		fprintf(stderr, "\n");
+	}
+	else if (tree->node_type == NODE_SUBSH) {
+		fprintf(stderr, "├── SUBSH\n");
+		display_tree_(((t_subsh *)tree)->cmdtree, ident_level + 1);
+	}
+	else if (tree->node_type == NODE_REDIR) {
+		t_redir *redir = (t_redir *)tree;
+		fprintf(stderr, "REDIR %d to ", redir->io_src);
+		if (redir->filenode->token & (WORD | VAR))
+			fprintf(stderr, "<%s: %s>", token_names[(int)log2(redir->filenode->token)], redir->filenode->val);
+		else {
+			t_node *cpy = redir->filenode->val_grp->top;
+			fprintf(stderr, " <GROUP: {");
+			while (cpy) {
+				fprintf(stderr, " <%s: %s> ", token_names[(int)log2(cpy->token)], cpy->val);
+				cpy = cpy->next;
+			}
+			fprintf(stderr, "}>");
+		}
+		fprintf(stderr, "\n");
+		display_tree_(redir->cmdtree, ident_level + 1);
+	}
+	else if (tree->node_type == NODE_PIPE || tree->node_type == NODE_FG
+		|| tree->node_type == NODE_BG || tree->node_type == NODE_AND
+		|| tree->node_type == NODE_OR)
+	{
+		fprintf(stderr, "├── %s\n", type[tree->node_type]);
+		display_tree_(((t_connector *)tree)->lcmdtree, ident_level + 1);
+		display_tree_(((t_connector *)tree)->rcmdtree, ident_level + 1);
+	}
+}
 
-// void	display_tree(t_cmdtree *tree)
-// {
-// 	fprintf(stderr, "-------------------Syntax Tree-------------------\n");
-// 	display_tree_(tree, 0);
-// 	fprintf(stderr, "-----------------End Syntax Tree-----------------\n");
-// }
+void	display_tree(t_cmdtree *tree)
+{
+	fprintf(stderr, "-------------------Syntax Tree-------------------\n");
+	display_tree_(tree, 0);
+	fprintf(stderr, "-----------------End Syntax Tree-----------------\n");
+}
