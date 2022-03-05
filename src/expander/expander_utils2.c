08a8bec51ebe0ace12225b270dfb724eb51a7384
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-03-05 17:11:41 +0100

    refactot: update expander_utils2
    - Add make_groups, concat_group, concat_words utils

--- src/expander/expander_utils2.c
+++ src/expander/expander_utils2.c
@@ -6,12 +6,75 @@
 /*   By: omoussao <omoussao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 16:32:25 by omoussao          #+#    #+#             */
-/*   Updated: 2022/03/05 16:48:06 by omoussao         ###   ########.fr       */
+/*   Updated: 2022/03/05 17:06:19 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	concat_group(t_node *node)
+{
+	t_node	*top;
+
+	top = node->val_grp->top;
+	if (top->token != WORD)
+		return ;
+	node->val = top->val;
+	top = top->next;
+	while (top)
+	{
+		if (!(top->token & (WORD | WILDC | TILDE)))
+			return ;
+		node->val = gc_filter(ft_strjoin(node->val, top->val), GC_TMP);
+		top = top->next;
+	}
+	node->token = WORD;
+}
+
+t_list	*concat_words(t_list *tokens)
+{
+	t_node	*top;
+
+	top = tokens->top;
+	while (top && top->token != ENDOFCMD)
+	{
+		if (top->token == GROUP)
+			concat_group(top);
+		top = top->next;
+	}
+	return (tokens);
+}
+
+t_list	*make_groups(t_list *tokens)
+{
+	t_node	*top;
+
+	top = tokens->top;
+	while (top && top->token != ENDOFCMD)
+	{
+		if (top->token & STRING)
+		{
+			while (top->next->token & STRING)
+			{
+				if (top->token != GROUP)
+				{
+					top->val_grp = new_list();
+					push_back(top->val_grp, top->token, top->val);
+					top->token = GROUP;
+					continue ;
+				}
+				push_back(top->val_grp, top->next->token, top->next->val);
+				del_node(tokens, top->next);
+			}
+		}
+		if (top->token == WSPACE)
+			del_node(tokens, top);
+		top = top->next;
+	}
+	return (tokens);
+}
+
+// Expander utils used in execution
 char	*expand_group(t_list *group)
 {
 	char	*ret;
